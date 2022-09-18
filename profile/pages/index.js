024530b8b436f4881e599d463dee55621d61e959
ananya1141 <ananya1141.be20@chitkarauniversity.edu.in>
2022-09-18 19:20:30 +0530

    all

---
+++ profile/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <body>
+      Hey this page is for ananya
+    </body>
+  )
+}
